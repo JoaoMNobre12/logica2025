1d2523caf5b3cb19100d0cfb9f782b4be454c3dd
João Marcos Nobre <149286129+JoaoMNobre12@users.noreply.github.com>
2025-07-10 19:41:54 -0300

    Create 1004 - Produto Simples.c

---
+++ 1004 - Produto Simples.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, PROD;
+
+    // Leitura dos dois inteiros
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    // Cálculo do produto
+    PROD = A * B;
+
+    // Impressão da saída no formato exigido
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
