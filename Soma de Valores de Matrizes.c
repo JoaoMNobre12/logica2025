cd05c8550284b451f1918167f2e437fafd10828a
João Marcos Nobre <149286129+JoaoMNobre12@users.noreply.github.com>
2025-07-25 10:13:42 -0300

    Update Soma de Valores de Matrizes.c

--- Soma de Valores de Matrizes.c
+++ Soma de Valores de Matrizes.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-int main (){
+int main ()
+{
     int Matriz[3][3];
     int i, j, Soma;
     
-    for (i=0; i<3; i++){
-        for (j=0; j<3; j++){
+    for (i=0; i<3; i++)
+    {
+        for (j=0; j<3; j++)
+        {
             printf("Digite o valor para a matriz [%d][%d]\n", i,j);
             scanf("%d", &Matriz[i][j]);
             Soma+= Matriz[i][j];
